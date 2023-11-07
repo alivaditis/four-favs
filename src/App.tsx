015beead508fdbee617a3f2a04cf6a6fadf2c731
Alec Livaditis <aleclivaditis@gmail.com>
2023-11-06 23:04:12 -0500

    style poster edit states

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import { Autocomplete, TextField, Paper } from '@mui/material';
+import { Autocomplete, TextField } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import edit from './imgs/edit-document.png'
+import close from './imgs/close.png'
+import edit from './imgs/pen.png'
 import add from './imgs/plus.png'
 import nullPoster from './imgs/null-poster.png'
 import './App.css';
@@ -27,82 +28,23 @@ type Movie = {
   video: boolean;
   vote_average: number;
   vote_count: number;
-};
+} | null;
 
 
 
 function App() {
-  const [favs, setFavs] = useState<(Movie | null)[]>([
-null,
-    {
-      "adult": false,
-      "backdrop_path": "/6N1uy1r66mXHefU3gG9ePZgijCW.jpg",
-      "genre_ids": [
-        28,
-        80,
-        18
-      ],
-      "id": 949,
-      "original_language": "en",
-      "original_title": "Heat",
-      "overview": "Obsessive master thief Neil McCauley leads a top-notch crew on various daring heists throughout Los Angeles while determined detective Vincent Hanna pursues him without rest. Each man recognizes and respects the ability and the dedication of the other even though they are aware their cat-and-mouse game may end in violence.",
-      "popularity": 48.584,
-      "poster_path": "/umSVjVdbVwtx5ryCA2QXL44Durm.jpg",
-      "release_date": "1995-12-15",
-      "title": "Heat",
-      "video": false,
-      "vote_average": 7.908,
-      "vote_count": 6618
-    },
-    {
-      "adult": false,
-      "backdrop_path": "/k7j5EQGs9fhW0kf7Ts0eN1FGEls.jpg",
-      "genre_ids": [
-        28,
-        14,
-        10749
-      ],
-      "id": 39915,
-      "original_language": "cn",
-      "original_title": "青蛇",
-      "overview": "A mischievous snake who assumes human form interferes with the romance between her reptilian sister and a hapless man.",
-      "popularity": 21.577,
-      "poster_path": "/qU5lu66AJFopMd5XxELxCkKzRRP.jpg",
-      "release_date": "1993-11-04",
-      "title": "Green Snake",
-      "video": false,
-      "vote_average": 7.1,
-      "vote_count": 83
-    },
-    {
-      "adult": false,
-      "backdrop_path": "/tAwgmXKnQ6WVczwcIq5LKFr0LDb.jpg",
-      "genre_ids": [
-        18
-      ],
-      "id": 25538,
-      "original_language": "zh",
-      "original_title": "一一",
-      "overview": "Each member of a family in Taipei asks hard questions about life's meaning as they live through everyday quandaries. NJ is morose: his brother owes him money, his mother is in a coma, his wife suffers a spiritual crisis when she finds her life a blank and his business partners make bad decisions.",
-      "popularity": 15.907,
-      "poster_path": "/hTPkCpK9SLGDMXRbUwzoep0MxOx.jpg",
-      "release_date": "2000-09-20",
-      "title": "Yi Yi",
-      "video": false,
-      "vote_average": 7.883,
-      "vote_count": 465
-    }
-  ])
+  const [favs, setFavs] = useState<(Movie)[]>([null, null, null, null])
+  const [editFavs, setEditFavs] = useState([...favs])
   const [options, setOptions] = useState<any[]>([])
   const [isEdit, setIsEdit] = useState(false)
   const [pos, setPos] = useState(0)
 
-  const handleValueChange = (event:any, newValue:any, pos:number) => {
+  const handleValueChange = (event:any, newValue:Movie, pos:number) => {
     if(newValue) {
-      const newFavs = [...favs]
+      const newFavs = [...editFavs]
       newFavs.splice(pos, 1, newValue)
       console.log(newFavs)
-      setFavs(newFavs)
+      setEditFavs(newFavs)
     }
   }
 
@@ -124,24 +66,29 @@ null,
     }
 
 
-  const posters = favs.map((fav, index) => {
-    return <div className='poster-box'>
-        {isEdit && 
+  const posters = (!isEdit ? favs : editFavs).map((fav, index) => {
+    return <div className={`poster-box ${fav && 'set-poster-hover'} ${isEdit && 'hovered-edit-poster'}`}>
+        {isEdit && fav &&
         <button
           className='remove-poster-button'
           onClick={() => {
-          const newFavs = [...favs]
+          const newFavs = [...editFavs]
           newFavs.splice(index, 1, null)
-          setFavs(newFavs)
+          setEditFavs(newFavs)
           }}
-          >
-          close
+        >
+          <img src={close}/>
         </button>}
         {isEdit &&
         <div className='poster-editing-icon'>
           <img src={fav ? edit : add} onClick={()=> setPos(index)}/>
         </div>}
-        <img className='poster' onClick={()=> setPos(index)} key={index} src={fav ? `https://image.tmdb.org/t/p/original/${fav.poster_path}` : nullPoster}/>
+        <div className='poster-overflow'>
+          <img
+            className='poster'
+            onClick={()=> setPos(index)} key={index} src={fav ? `https://image.tmdb.org/t/p/original/${fav.poster_path}` : nullPoster}
+          />
+        </div>
       </div>
   })
 
@@ -156,6 +103,29 @@ null,
         <div className="favs-container">
           {posters}
         </div>
+        {isEdit && 
+        <>
+          <button onClick={()=>setIsEdit(false)}>Cancel</button>
+          <button 
+            onClick={()=> {
+              const newFavs = [...editFavs]
+              newFavs.sort((a, b) => {
+                if (a === null && b !== null) {
+                  return 1
+                }
+                if (a !== null && b === null) {
+                  return -1
+                }
+                return 0
+              })
+              setFavs([...newFavs])
+              setEditFavs([...newFavs])
+              setIsEdit(false)
+            }}>
+            Save
+          </button>
+        </>
+        }
         <ThemeProvider theme={darkTheme}>
           <Autocomplete
             disablePortal
