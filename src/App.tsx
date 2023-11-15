e0b564f84c7adda3dfbd82fc6a8738ea77789443
Alec Livaditis <aleclivaditis@gmail.com>
2023-11-14 22:28:49 -0500

    add Home component that redirects to demo1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
 import Favs from './components/Favs/Favs'
 
 import './App.css';
@@ -6,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path='/:username' element={<Favs/>}/>
     </Routes>
   )
